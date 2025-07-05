38462f7b6375bcf9382ef02fccd6cae8cc7dcd21
Lakshay1838 <lakshaypahuja777@gmail.com>
2025-07-05 21:47:09 +0530

    setting up backend for new project MilestoneTracker

---
+++ milestoneBackend/milestoneBackend/src/main/java/com/example/milestoneBackend/MilestoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.milestoneBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilestoneBackendApplication.class, args);
+	}
+
+}
---
+++ milestoneBackend/milestoneBackend/src/test/java/com/example/milestoneBackend/MilestoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.milestoneBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilestoneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
